fa0d7d5c618ab0f0e6b308cf65e6526f2d194bac
Junek3 <rlarudrms28@gmail.com>
2021-09-18 23:40:48 +0900

    add simple view files

--- src/main/java/RootLab/Spring101/helloSpring/controller/MemberController.java
+++ src/main/java/RootLab/Spring101/helloSpring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package RootLab.Spring101.helloSpring.controller;
 
+import RootLab.Spring101.helloSpring.domain.Member;
 import RootLab.Spring101.helloSpring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -11,5 +17,25 @@ public class MemberController {
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+
+	@GetMapping("/members/new")
+	public String createForm(){
+		return "members/createMemberForm";
+	}
+
+	@PostMapping("/members/new")
+	public String createMember(MemberForm form){
+		Member member = new Member();
+		member.setName(form.getName());
+		memberService.join(member);
+		return "redirect:/";
+	}
+
+	@GetMapping("/members")
+	public String memberList(Model model){
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
 	}
 }
