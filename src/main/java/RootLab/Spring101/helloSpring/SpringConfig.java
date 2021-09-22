d124b77c669169d24e7e09bd0f204b8e952652bd
Junek3 <rlarudrms28@gmail.com>
2021-09-22 12:51:38 +0900

    connect JdbcMemberRepository

--- src/main/java/RootLab/Spring101/helloSpring/SpringConfig.java
+++ src/main/java/RootLab/Spring101/helloSpring/SpringConfig.java
@@ -1,20 +1,29 @@
 package RootLab.Spring101.helloSpring;
 
+import RootLab.Spring101.helloSpring.repository.JdbcMemberRepository;
 import RootLab.Spring101.helloSpring.repository.MemberRepository;
 import RootLab.Spring101.helloSpring.repository.MemoryMemberRepository;
 import RootLab.Spring101.helloSpring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+	@Autowired
+	private DataSource dataSource;
+
 	@Bean
-	public MemberService memberService(){
+	public MemberService memberService() {
 		return new MemberService(memberRepository());
 	}
 
 	@Bean
-	public MemberRepository memberRepository(){
-		return new MemoryMemberRepository();
+	public MemberRepository memberRepository() {
+//		return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 }
